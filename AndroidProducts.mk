6b724113c16bea171359b0829de075c8d2eadac8
Ekorudianto27 <Layeardteam@gmail.com>
2020-12-18 10:51:44 +0000

    Ancient-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,6 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_a6000.mk
+    $(LOCAL_DIR)/ancient_a6000.mk
 
 # Lunch choices
 COMMON_LUNCH_CHOICES := \
-    lineage_a6000-userdebug
+    ancient_a6000-userdebug
